eb38db86207b5288e66710b293a165903f2396f6
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-05-24 20:54:43 +0300

    fixes

--- CQueue.py
+++ CQueue.py
@@ -2,30 +2,31 @@ from CResident import CResident
 from typing import List, Any
 from numpy import random
 
+
 class CQueue(object):
 
-    def __init__(self, max_people_in_queue:int, probabilities_to_stay:dict ):
+    def __init__(self, max_people_in_queue: int, probabilities_to_stay: dict):
         self.max_people_in_queue = max_people_in_queue
         self.curr_people_in_queue = 0
         self.probabilities_to_stay_per_amount_of_people = probabilities_to_stay
-        self.time_per_amount_of_people = dict.fromkeys(range(max_people_in_queue),0)
+        self.time_per_amount_of_people = dict.fromkeys(range(max_people_in_queue), 0)
         self.curr_service_time = 0
         self.wait_time_list = 0
         self.service_time_list = 0
         self.time_per_amount_of_people = {}
-        self.residents_queue:List[CResident] = []
+        self.residents_queue: List[CResident] = []
 
-    def addResidentToTheQueue(self, resident:CResident)->bool:
-        if(self.curr_people_in_queue == self.max_people_in_queue):
+    def addResidentToTheQueue(self, resident: CResident) -> bool:
+        if (self.curr_people_in_queue == self.max_people_in_queue):
             return False
 
-        range = self.probabilities_to_stay_per_amount_of_people[self.curr_people_in_queue]*10
-        if(range == 10.0):
+        range = self.probabilities_to_stay_per_amount_of_people[self.curr_people_in_queue] * 10
+        if (range == 10.0):
             self.residents_queue.append(resident)
             self.curr_people_in_queue += 1
             return True
 
-        if( random.random_integers(1,10,1)[0] >= range ):
+        if (random.random_integers(1, 10, 1)[0] >= range):
             return False
 
         else:
@@ -43,6 +44,9 @@ class CQueue(object):
     def getResidentsQueue(self):
         return self.residents_queue
 
-    def addWaitingTimeToResidents(self,time):
+    def getServedResident(self):
+        return self.residents_queue[0]
+
+    def addWaitingTimeToResidents(self, time):
         for resident in self.residents_queue:
             resident.waiting_time += time
--- main.py
+++ main.py
@@ -13,40 +13,37 @@ if __name__ == '__main__':
     probabilities = dict()
 
     i = 5
-    while( i < len(sys.argv)):
-        probabilities[i-5] = float(sys.argv[i])
+    while (i < len(sys.argv)):
+        probabilities[i - 5] = float(sys.argv[i])
         i += 1
 
-    system = CSystem(T,M,probabilities)
+    system = CSystem(T, M, probabilities)
 
-    while(True):
+    while (True):
         arrived_time = np.random.poisson(lamda)
         system.addTimeTicks(arrived_time)
-        chosenQueue = np.random.random_integers(1,M,1)[0]
+        chosenQueue = np.random.random_integers(1, M, 1)[0]
         systemQueues = system.getQueues()
         resident = CResident()
-        status = systemQueues[chosenQueue].addResidentToTheQueue(resident)
-        if(status == False):
-            system.numberOfLeft += 1
-        else:
-            system.numberOfStayed += 1
+        stayed = systemQueues[chosenQueue].addResidentToTheQueue(resident)
+        system.personStayed if stayed else system.personLeft
 
         for queue in system.getQueues():
-            if(queue.curr_pepole_in_queue == 0):
+            if (queue.curr_pepole_in_queue == 0):
                 continue
             else:
-                if(queue.curr_service_time > 0):
+                if (queue.curr_service_time > 0 and queue.curr_service_time <= arrived_time):
+                    queue.getServedResident().addTimeInService(queue.curr_service_time)
                     queue.removeResidentFromTheQueue()
-                    queue.addTimeInService(queue.curr_service_time)
                 time_to_add_from_last_iteration = queue.curr_service_time
                 service_time = np.random.poisson(miu)
                 queue.curr_service_time = service_time
-                queue.getResidentsQueue()[0].service_time = queue.curr_service_time
-                while(service_time + time_to_add_from_last_iteration <= arrived_time):
+                queue.getServedResident().addTimeInService(queue.curr_service_time)
+                while (service_time + time_to_add_from_last_iteration <= arrived_time):
                     queue.removeResidentFromTheQueue()
                     queue.addTimeInService(queue.curr_service_time)
                     service_time = np.random.poisson(miu)
-                    queue.getResidentsQueue()[0].service_time = service_time
+                    queue.getServedResident().service_time = service_time
                     service_time += service_time + queue.curr_service_time
                 delta = service_time - arrived_time;
                 queue.curr_service_time = delta
